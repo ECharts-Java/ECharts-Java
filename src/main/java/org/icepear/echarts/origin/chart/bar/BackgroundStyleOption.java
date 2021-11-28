b09990bf6cc377704cd1ebbc3c4aca217af7f8a7
Zhexin Jin <icepear.jzx@gmail.com>
2021-11-28 04:15:37 -0500

    test Bar with Background

---
+++ src/main/java/org/icepear/echarts/origin/chart/bar/BackgroundStyleOption.java
@@ -0,0 +1,9 @@
+package org.icepear.echarts.origin.chart.bar;
+
+import java.util.List;
+
+import org.icepear.echarts.origin.util.ItemStyleOption;
+
+public interface BackgroundStyleOption extends ItemStyleOption {
+    void setBorderRadius(List<Number> borderRadius);
+}
---
+++ src/main/java/org/icepear/echarts/origin/util/ItemStyleOption.java
@@ -0,0 +1,12 @@
+package org.icepear.echarts.origin.util;
+
+/**
+ * https://github.com/apache/echarts/blob/790687df55a5dbe286e52cf182c0983938efd367/src/util/types.ts#L971
+ */
+public interface ItemStyleOption extends ShadowOptionMixin, BorderOptionMixin {
+    void setColor(String color);
+
+    void setOpacity(Number opacity);
+    
+    void setDecal(Object decal);
+}
