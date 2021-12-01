80628afd34a7d3590aaad3566be10fed3e7a7462
Zhexin Jin <icepear.jzx@gmail.com>
2021-12-01 01:59:01 -0500

    add LineStyle

--- src/main/java/org/icepear/echarts/component/LineStyle.java
+++ src/main/java/org/icepear/echarts/component/LineStyle.java
@@ -1,16 +1,28 @@
 package org.icepear.echarts.component;
 
-import org.icepear.echarts.origin.util.AreaStyleOption;
+import org.icepear.echarts.origin.util.LineStyleOption;
 
 import lombok.experimental.Accessors;
 import lombok.Data;
 
 @Accessors(chain=true)
 @Data
-public class AreaStyle implements AreaStyleOption {
+public class LineStyle implements LineStyleOption {
+    private Number width;
+
     private String color;
 
     private Number opacity;
+
+    private String type;
+
+    private Object cap;
+
+    private Object join;
+
+    private Number dashOffset;
+
+    private Number miterLimit;
 
     private Number shadowBlur;
 
---
+++ src/main/java/org/icepear/echarts/origin/util/LineStyleOption.java
@@ -0,0 +1,22 @@
+package org.icepear.echarts.origin.util;
+
+/**
+ * https://github.com/apache/echarts/blob/790687df55a5dbe286e52cf182c0983938efd367/src/util/types.ts#L982
+ */
+public interface LineStyleOption extends ShadowOptionMixin {
+    LineStyleOption setWidth(Number width);
+
+    LineStyleOption setColor(String color);
+
+    LineStyleOption setOpacity(Number opacity);
+
+    LineStyleOption setType(String type);
+
+    LineStyleOption setCap(Object cap);
+
+    LineStyleOption setJoin(Object join);
+
+    LineStyleOption setDashOffset(Number dashOffset);
+
+    LineStyleOption setMiterLimit(Number miterLimit);
+}
