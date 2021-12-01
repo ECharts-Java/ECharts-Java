7596c0d1e068bdfec67b4aa820d79c4ce16fd80e
Zhexin Jin <icepear.jzx@gmail.com>
2021-12-01 01:59:01 -0500

    add LineStyle

--- src/main/java/org/icepear/echarts/origin/util/AreaStyleOption.java
+++ src/main/java/org/icepear/echarts/origin/util/AreaStyleOption.java
@@ -3,7 +3,7 @@ package org.icepear.echarts.origin.util;
 /**
  * https://github.com/apache/echarts/blob/790687df55a5dbe286e52cf182c0983938efd367/src/util/types.ts#L997
  */
-public interface AreaStyleOption {
+public interface AreaStyleOption extends ShadowOptionMixin {
     AreaStyleOption setColor(String color);
 
     AreaStyleOption setOpacity(Number opacity);
