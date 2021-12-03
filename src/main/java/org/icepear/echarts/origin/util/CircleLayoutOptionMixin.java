3cefa1f780fe98980274cfbb0b3d199de8c7900a
Zhexin Jin <icepear.jzx@gmail.com>
2021-12-02 22:12:43 -0500

    test Two Value-Axes in Polar 2

--- src/main/java/org/icepear/echarts/origin/util/CircleLayoutOptionMixin.java
+++ src/main/java/org/icepear/echarts/origin/util/CircleLayoutOptionMixin.java
@@ -1,12 +1,14 @@
 package org.icepear.echarts.origin.util;
 
+import java.util.List;
+
 import org.icepear.echarts.type.EChartsNumber;
 
 /**
  * https://github.com/apache/echarts/blob/790687df55a5dbe286e52cf182c0983938efd367/src/util/types.ts#L834
  */
 public interface CircleLayoutOptionMixin {
-    CircleLayoutOptionMixin setCenter(EChartsNumber center);
+    CircleLayoutOptionMixin setCenter(List<EChartsNumber> center);
 
-    CircleLayoutOptionMixin setRadius(EChartsNumber radius);
+    CircleLayoutOptionMixin setRadius(List<EChartsNumber>  radius);
 }
