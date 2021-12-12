3d5c0aabc27eff2679e070b2c78ad5830f53fc20
Xiaochuan Xu <xiaochuanxu@cmu.edu>
2021-12-12 11:41:54 -0500

    add datazoom, visualmap & extend typescript-config to include more types

---
+++ src/main/java/org/icepear/echarts/origin/component/visualMap/ContinousVisualMapOption.java
@@ -0,0 +1,39 @@
+package org.icepear.echarts.origin.component.visualMap;
+
+import org.icepear.echarts.origin.util.ItemStyleOption;
+
+/**
+ * https://github.com/apache/echarts/blob/790687df55a5dbe286e52cf182c0983938efd367/src/component/visualMap/ContinuousModel.ts#L36
+ */
+public interface ContinousVisualMapOption extends VisualMapOption {
+
+	ContinousVisualMapOption setAlign(String align);
+
+	ContinousVisualMapOption setCalculable(Boolean calculable);
+
+	ContinousVisualMapOption setRange(Number[] range);
+
+	ContinousVisualMapOption setHoverLink(Boolean hoverLink);
+
+	ContinousVisualMapOption setHoverLinkDataSize(Number hoverLinkDataSize);
+
+	ContinousVisualMapOption setHoverLinkOnHandle(Boolean hoverLinkOnHandle);
+
+	ContinousVisualMapOption setHandleIcon(String handleIcon);
+
+	ContinousVisualMapOption setHandleSize(Number handleSize);
+
+	ContinousVisualMapOption setHandleSize(String handleSize);
+
+	ContinousVisualMapOption setHandleStyle(ItemStyleOption handleStyle);
+
+	ContinousVisualMapOption setIndicatorIcon(String indicatorIcon);
+
+	ContinousVisualMapOption setIndicatorSize(Number indicatorSize);
+
+	ContinousVisualMapOption setIndicatorSize(String indicatorSize);
+
+	ContinousVisualMapOption setIndicatorStyle(ItemStyleOption indicatorStyle);
+
+	ContinousVisualMapOption setEmphasis(Object emphasis);
+}
---
+++ src/main/java/org/icepear/echarts/origin/component/visualMap/VisualMapOption.java
@@ -0,0 +1,70 @@
+package org.icepear.echarts.origin.component.visualMap;
+
+import org.icepear.echarts.origin.util.BorderOptionMixin;
+import org.icepear.echarts.origin.util.BoxLayoutOptionMixin;
+import org.icepear.echarts.origin.util.ComponentOption;
+import org.icepear.echarts.origin.util.LabelOption;
+
+/**
+ * https://github.com/apache/echarts/blob/4569dc1d8b712dafb80b16e31c4fed0118e3acbd/src/component/visualMap/VisualMapModel.ts#L56
+ */
+public interface VisualMapOption extends ComponentOption, BoxLayoutOptionMixin, BorderOptionMixin {
+
+	VisualMapOption setMainType(String mainType);
+
+	VisualMapOption setShow(Boolean show);
+
+	VisualMapOption setAlign(String align);
+
+	VisualMapOption setRealtime(Boolean realtime);
+
+	VisualMapOption setSeriesIndex(Number seriesIndex);
+
+	VisualMapOption setSeriesIndex(Number[] seriesIndex);
+
+	VisualMapOption setSeriesIndex(String seriesIndex);
+
+	VisualMapOption setMin(Number min);
+
+	VisualMapOption setMax(Number max);
+
+	VisualMapOption setDimension(Number dimension);
+
+	VisualMapOption setController(Object controller);
+
+	VisualMapOption setTarget(Object target);
+
+	VisualMapOption setItemWidth(Number itemWidth);
+
+	VisualMapOption setItemHeight(Number itemHeight);
+
+	VisualMapOption setInverse(Boolean inverse);
+
+	VisualMapOption setOrient(String orient);
+
+	VisualMapOption setBackgroundColor(String backgroundColor);
+
+	VisualMapOption setContentColor(String contentColor);
+
+	VisualMapOption setInactiveColor(String inactiveColor);
+
+	VisualMapOption setPadding(Number padding);
+
+	VisualMapOption setPadding(Number[] padding);
+
+	VisualMapOption setTextGap(Number textGap);
+
+	VisualMapOption setPrecision(Number precision);
+
+	VisualMapOption setColor(String[] color);
+
+	VisualMapOption setFormatter(Object formatter);
+
+	VisualMapOption setFormatter(String formatter);
+
+	VisualMapOption setText(String[] text);
+
+	VisualMapOption setTextStyle(LabelOption textStyle);
+
+	VisualMapOption setCategories(Object categories);
+}
