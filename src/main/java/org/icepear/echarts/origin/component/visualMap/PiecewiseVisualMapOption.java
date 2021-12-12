e0b7fcdf9d797692c820e7c10ec865bbcda440fe
Xiaochuan Xu <xiaochuanxu@cmu.edu>
2021-12-12 11:41:54 -0500

    add datazoom, visualmap & extend typescript-config to include more types

---
+++ src/main/java/org/icepear/echarts/origin/component/visualMap/PiecewiseVisualMapOption.java
@@ -0,0 +1,35 @@
+package org.icepear.echarts.origin.component.visualMap;
+
+/**
+ * https://github.com/apache/echarts/blob/0114414eed6d0f9d1bb951459517e21ae557989f/src/component/visualMap/PiecewiseModel.ts#L70
+ */
+public interface PiecewiseVisualMapOption extends VisualMapOption {
+
+	PiecewiseVisualMapOption setAlign(String align);
+
+	PiecewiseVisualMapOption setMinOpen(Boolean minOpen);
+
+	PiecewiseVisualMapOption setMaxOpen(Boolean maxOpen);
+
+	PiecewiseVisualMapOption setItemWidth(Number itemWidth);
+
+	PiecewiseVisualMapOption setItemHeight(Number itemHeight);
+
+	PiecewiseVisualMapOption setItemSymbol(String itemSymbol);
+
+	PiecewiseVisualMapOption setPieces(Object[] pieces);
+
+	PiecewiseVisualMapOption setCategories(String[] categories);
+
+	PiecewiseVisualMapOption setSplitNumber(Number splitNumber);
+
+	PiecewiseVisualMapOption setSelected(Object selected);
+
+	PiecewiseVisualMapOption setSelectedMode(String selectedMode);
+
+	PiecewiseVisualMapOption setShowLabel(Boolean showLabel);
+
+	PiecewiseVisualMapOption setItemGap(Number itemGap);
+
+	PiecewiseVisualMapOption setHoverLink(Boolean hoverLink);
+}
