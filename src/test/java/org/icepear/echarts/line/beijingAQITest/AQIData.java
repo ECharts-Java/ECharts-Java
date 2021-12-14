2298b5e0d563ba9564e7eb256a7ebc41ea36b87d
Zhexin Jin <icepear.jzx@gmail.com>
2021-12-14 16:14:45 -0500

    format

--- src/test/java/org/icepear/echarts/line/beijingAQITest/AQIData.java
+++ src/test/java/org/icepear/echarts/line/beijingAQITest/AQIData.java
@@ -7,7 +7,7 @@ class AQIData {
     private String date;
     private Number aqi;
 
-    public AQIData(String date, Number aqi){
+    public AQIData(String date, Number aqi) {
         this.date = date;
         this.aqi = aqi;
     }
