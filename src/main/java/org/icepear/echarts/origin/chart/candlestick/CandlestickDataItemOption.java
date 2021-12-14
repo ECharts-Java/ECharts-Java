3581f105fbe1deb81cf9d8598d656e228b81a46d
Zhexin Jin <icepear.jzx@gmail.com>
2021-12-14 00:55:13 -0500

    finish candlestick

---
+++ src/main/java/org/icepear/echarts/origin/chart/candlestick/CandlestickDataItemOption.java
@@ -0,0 +1,11 @@
+package org.icepear.echarts.origin.chart.candlestick;
+
+import org.icepear.echarts.origin.util.StatesOptionMixin;
+
+/**
+ * https://github.com/apache/echarts/blob/1277d7ae81f2b1c7ede4e1731820a1c06bd16cf0/src/chart/candlestick/CandlestickSeries.ts#L51
+ */
+public interface CandlestickDataItemOption extends CandlestickStateOption, StatesOptionMixin {
+
+	CandlestickDataItemOption setValue(Object value);
+}
---
+++ src/main/java/org/icepear/echarts/origin/chart/candlestick/CandlestickItemStyleOption.java
@@ -0,0 +1,13 @@
+package org.icepear.echarts.origin.chart.candlestick;
+
+import org.icepear.echarts.origin.util.ItemStyleOption;
+
+/**
+ * https://github.com/apache/echarts/blob/1277d7ae81f2b1c7ede4e1731820a1c06bd16cf0/src/chart/candlestick/CandlestickSeries.ts#L43
+ */
+public interface CandlestickItemStyleOption extends ItemStyleOption {
+
+	CandlestickItemStyleOption setColor0(Object color0);
+
+	CandlestickItemStyleOption setBorderColor0(Object borderColor0);
+}
