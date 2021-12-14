a6a110de41b4bf3b6663110f8566afb0e2471b4f
Zhexin Jin <icepear.jzx@gmail.com>
2021-12-14 00:38:10 -0500

    finish scatter

---
+++ src/main/java/org/icepear/echarts/origin/util/SeriesOnSingleOptionMixin.java
@@ -0,0 +1,11 @@
+package org.icepear.echarts.origin.util;
+
+/**
+ * https://github.com/apache/echarts/blob/1277d7ae81f2b1c7ede4e1731820a1c06bd16cf0/src/util/types.ts#L1682
+ */
+public interface SeriesOnSingleOptionMixin {
+
+	SeriesOnSingleOptionMixin setSingleAxisIndex(Number singleAxisIndex);
+
+	SeriesOnSingleOptionMixin setSingleAxisId(String singleAxisId);
+}
