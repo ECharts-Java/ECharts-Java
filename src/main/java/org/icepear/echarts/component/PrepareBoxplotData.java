565b98f72d4c60f1d48fe6fbc50ccc34560795c5
Zhexin Jin <icepear.jzx@gmail.com>
2021-12-14 02:50:54 -0500

    finish boxplot

---
+++ src/main/java/org/icepear/echarts/component/PrepareBoxplotData.java
@@ -0,0 +1,28 @@
+package org.icepear.echarts.component;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import org.icepear.echarts.origin.chart.boxplot.PrepareBoxplotDataOption;
+
+@Accessors(chain=true)
+@Data
+public class PrepareBoxplotData implements PrepareBoxplotDataOption {
+
+	@Setter(AccessLevel.NONE)
+	private Object boundIQR;
+
+	public PrepareBoxplotData setBoundIQR(Number boundIQR) {
+		this.boundIQR = boundIQR;
+		return this;
+	}
+
+	public PrepareBoxplotData setBoundIQR(String boundIQR) {
+		this.boundIQR = boundIQR;
+		return this;
+	}
+
+	private String itemNameFormatter;
+}
