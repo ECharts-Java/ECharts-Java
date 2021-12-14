b356697142b2f586851a35f681caea7591ee1b68
Zhexin Jin <icepear.jzx@gmail.com>
2021-12-14 00:55:13 -0500

    finish candlestick

---
+++ src/main/java/org/icepear/echarts/origin/chart/candlestick/CandlestickStateOption.java
@@ -0,0 +1,13 @@
+package org.icepear.echarts.origin.chart.candlestick;
+
+import org.icepear.echarts.origin.util.SeriesLabelOption;
+
+/**
+ * https://github.com/apache/echarts/blob/1277d7ae81f2b1c7ede4e1731820a1c06bd16cf0/src/chart/candlestick/CandlestickSeries.ts#L47
+ */
+public interface CandlestickStateOption {
+
+	CandlestickStateOption setItemStyle(CandlestickItemStyleOption itemStyle);
+
+	CandlestickStateOption setLabel(SeriesLabelOption label);
+}
