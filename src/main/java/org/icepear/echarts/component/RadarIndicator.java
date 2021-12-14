edb7dabcdc2d80390c83604353307d2f43149070
Zhexin Jin <icepear.jzx@gmail.com>
2021-12-14 01:55:10 -0500

    finish radar

---
+++ src/main/java/org/icepear/echarts/component/RadarIndicator.java
@@ -0,0 +1,23 @@
+package org.icepear.echarts.component;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import org.icepear.echarts.origin.coord.radar.RadarIndicatorOption;
+
+@Accessors(chain=true)
+@Data
+public class RadarIndicator implements RadarIndicatorOption {
+
+	private String name;
+
+	private String text;
+
+	private Number min;
+
+	private Number max;
+
+	private Object color;
+
+	private String axisType;
+}
