db068be512d8ca8b20722e41c9b879982521e8b1
Zhexin Jin <icepear.jzx@gmail.com>
2021-12-14 00:38:10 -0500

    finish scatter

---
+++ src/main/java/org/icepear/echarts/origin/chart/scatter/ScatterStateOption.java
@@ -0,0 +1,14 @@
+package org.icepear.echarts.origin.chart.scatter;
+
+import org.icepear.echarts.origin.util.ItemStyleOption;
+import org.icepear.echarts.origin.util.SeriesLabelOption;
+
+/**
+ * https://github.com/apache/echarts/blob/1277d7ae81f2b1c7ede4e1731820a1c06bd16cf0/src/chart/scatter/ScatterSeries.ts#L45
+ */
+public interface ScatterStateOption {
+
+	ScatterStateOption setItemStyle(ItemStyleOption itemStyle);
+
+	ScatterStateOption setLabel(SeriesLabelOption label);
+}
---
+++ src/main/java/org/icepear/echarts/origin/util/SeriesOnCalendarOptionMixin.java
@@ -0,0 +1,11 @@
+package org.icepear.echarts.origin.util;
+
+/**
+ * https://github.com/apache/echarts/blob/1277d7ae81f2b1c7ede4e1731820a1c06bd16cf0/src/util/types.ts#L1692
+ */
+public interface SeriesOnCalendarOptionMixin {
+
+	SeriesOnCalendarOptionMixin setCalendarIndex(Number calendarIndex);
+
+	SeriesOnCalendarOptionMixin setCalendarId(String calendarId);
+}
