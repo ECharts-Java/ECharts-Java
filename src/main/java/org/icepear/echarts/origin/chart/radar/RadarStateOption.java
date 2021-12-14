14b9e3785b79b21d2f5cd9d693b687a63f02eb02
Zhexin Jin <icepear.jzx@gmail.com>
2021-12-14 01:55:10 -0500

    finish radar

---
+++ src/main/java/org/icepear/echarts/origin/chart/radar/RadarStateOption.java
@@ -0,0 +1,20 @@
+package org.icepear.echarts.origin.chart.radar;
+
+import org.icepear.echarts.origin.util.AreaStyleOption;
+import org.icepear.echarts.origin.util.ItemStyleOption;
+import org.icepear.echarts.origin.util.LineStyleOption;
+import org.icepear.echarts.origin.util.SeriesLabelOption;
+
+/**
+ * https://github.com/apache/echarts/blob/1277d7ae81f2b1c7ede4e1731820a1c06bd16cf0/src/chart/radar/RadarSeries.ts#L50
+ */
+public interface RadarStateOption {
+
+	RadarStateOption setLineStyle(LineStyleOption lineStyle);
+
+	RadarStateOption setAreaStyle(AreaStyleOption areaStyle);
+
+	RadarStateOption setLabel(SeriesLabelOption label);
+
+	RadarStateOption setItemStyle(ItemStyleOption itemStyle);
+}
---
+++ src/main/java/org/icepear/echarts/origin/coord/radar/RadarIndicatorOption.java
@@ -0,0 +1,19 @@
+package org.icepear.echarts.origin.coord.radar;
+
+/**
+ * https://github.com/apache/echarts/blob/1277d7ae81f2b1c7ede4e1731820a1c06bd16cf0/src/coord/radar/RadarModel.ts#L44
+ */
+public interface RadarIndicatorOption {
+
+	RadarIndicatorOption setName(String name);
+
+	RadarIndicatorOption setText(String text);
+
+	RadarIndicatorOption setMin(Number min);
+
+	RadarIndicatorOption setMax(Number max);
+
+	RadarIndicatorOption setColor(Object color);
+
+	RadarIndicatorOption setAxisType(String axisType);
+}
