a3efb625ebee7eba9791b128ae2b15a5544f8002
Zhexin Jin <icepear.jzx@gmail.com>
2021-12-14 00:55:13 -0500

    finish candlestick

---
+++ src/main/java/org/icepear/echarts/component/CandlestickItemStyle.java
@@ -0,0 +1,56 @@
+package org.icepear.echarts.component;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import org.icepear.echarts.origin.chart.candlestick.CandlestickItemStyleOption;
+
+@Accessors(chain=true)
+@Data
+public class CandlestickItemStyle implements CandlestickItemStyleOption {
+
+	private Number shadowBlur;
+
+	private String shadowColor;
+
+	private Number shadowOffsetX;
+
+	private Number shadowOffsetY;
+
+	private String borderColor;
+
+	private Number borderWidth;
+
+	private String borderType;
+
+	private Object borderCap;
+
+	private Object borderJoin;
+
+	private Number borderDashOffset;
+
+	private Number borderMiterLimit;
+
+	private String color;
+
+	private Number opacity;
+
+	@Setter(AccessLevel.NONE)
+	private Object decal;
+
+	public CandlestickItemStyle setDecal(Object decal) {
+		this.decal = decal;
+		return this;
+	}
+
+	public CandlestickItemStyle setDecal(String decal) {
+		this.decal = decal;
+		return this;
+	}
+
+	private Object color0;
+
+	private Object borderColor0;
+}
---
+++ src/main/java/org/icepear/echarts/origin/chart/candlestick/CandlestickSeriesOption.java
@@ -0,0 +1,36 @@
+package org.icepear.echarts.origin.chart.candlestick;
+
+import org.icepear.echarts.origin.util.SeriesEncodeOptionMixin;
+import org.icepear.echarts.origin.util.SeriesLargeOptionMixin;
+import org.icepear.echarts.origin.util.SeriesOnCartesianOptionMixin;
+import org.icepear.echarts.origin.util.SeriesOption;
+
+/**
+ * https://github.com/apache/echarts/blob/1277d7ae81f2b1c7ede4e1731820a1c06bd16cf0/src/chart/candlestick/CandlestickSeries.ts#L63
+ */
+public interface CandlestickSeriesOption extends SeriesOption, CandlestickStateOption, SeriesOnCartesianOptionMixin, SeriesLargeOptionMixin, SeriesEncodeOptionMixin {
+
+	CandlestickSeriesOption setType(String type);
+
+	CandlestickSeriesOption setCoordinateSystem(String coordinateSystem);
+
+	CandlestickSeriesOption setLayout(Object layout);
+
+	CandlestickSeriesOption setClip(Boolean clip);
+
+	CandlestickSeriesOption setBarMaxWidth(Number barMaxWidth);
+
+	CandlestickSeriesOption setBarMaxWidth(String barMaxWidth);
+
+	CandlestickSeriesOption setBarMinWidth(Number barMinWidth);
+
+	CandlestickSeriesOption setBarMinWidth(String barMinWidth);
+
+	CandlestickSeriesOption setBarWidth(Number barWidth);
+
+	CandlestickSeriesOption setBarWidth(String barWidth);
+
+	CandlestickSeriesOption setData(CandlestickDataItemOption[] data);
+
+	CandlestickSeriesOption setData(Object[] data);
+}
