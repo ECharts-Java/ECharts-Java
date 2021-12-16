3cf9bc0638cfbc8551201291af46f6210a672cbc
Zhexin Jin <icepear.jzx@gmail.com>
2021-12-16 18:40:52 -0500

    finish heatmap

--- src/main/java/org/icepear/echarts/origin/component/visualMap/PiecewiseVisualMapOption.java
+++ src/main/java/org/icepear/echarts/origin/component/visualMap/PiecewiseVisualMapOption.java
@@ -1,5 +1,7 @@
 package org.icepear.echarts.origin.component.visualMap;
 
+import org.icepear.echarts.component.VisualPiece;
+
 /**
  * https://github.com/apache/echarts/blob/0114414eed6d0f9d1bb951459517e21ae557989f/src/component/visualMap/PiecewiseModel.ts#L70
  */
@@ -17,7 +19,7 @@ public interface PiecewiseVisualMapOption extends VisualMapOption {
 
 	PiecewiseVisualMapOption setItemSymbol(String itemSymbol);
 
-	PiecewiseVisualMapOption setPieces(Object[] pieces);
+	PiecewiseVisualMapOption setPieces(VisualPiece[] pieces);
 
 	PiecewiseVisualMapOption setCategories(String[] categories);
 
