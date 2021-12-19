d796cc0751c8bfe509d17daabaac928d26079f96
Zhexin Jin <icepear.jzx@gmail.com>
2021-12-19 02:29:13 -0500

    move tests

---
+++ src/test/java/org/icepear/echarts/advanced/candlestick/BasicCandlestickTest.java
@@ -0,0 +1,48 @@
+package org.icepear.echarts.advanced.candlestick;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import org.icepear.echarts.Option;
+import org.icepear.echarts.charts.candlestick.CandlestickSeries;
+import org.icepear.echarts.components.coord.cartesian.CategoryAxis;
+import org.icepear.echarts.components.coord.cartesian.DefaultAxis;
+import org.icepear.echarts.origin.util.SeriesOption;
+import org.icepear.echarts.serializer.EChartSerializer;
+import org.junit.Test;
+
+public class BasicCandlestickTest {
+    @Test
+    public void testBasicCandlestick() {
+        CategoryAxis xAxis = new CategoryAxis()
+                .setData(new String[] { "2017-10-24", "2017-10-25", "2017-10-26", "2017-10-27" });
+
+        DefaultAxis yAxis = new DefaultAxis();
+
+        CandlestickSeries series = new CandlestickSeries()
+                .setType("candlestick")
+                .setData(new Number[][] {
+                        { 20, 34, 10, 38 },
+                        { 40, 35, 30, 50 },
+                        { 31, 38, 33, 44 },
+                        { 38, 15, 5, 42 } });
+
+        Option option = new Option()
+                .setXAxis(xAxis)
+                .setYAxis(yAxis)
+                .setSeries(new SeriesOption[] { series });
+
+        Reader reader = new InputStreamReader(
+                this.getClass().getResourceAsStream("/candlestick/basic-candlestick.json"));
+        JsonElement expected = JsonParser.parseReader(reader);
+        JsonElement actual = EChartSerializer.toJsonTree(option);
+        assertEquals(expected, actual);
+
+        // System.out.println(EChartSerializer.toJson(option));
+    }
+}
