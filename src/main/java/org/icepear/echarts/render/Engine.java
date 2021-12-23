6b8bcd7790b7357efc8572468b24ec1359fbc39e
Xiaochuan Xu <xiaochuanxu@cmu.edu>
2021-12-22 23:17:04 -0500

    directory change

--- src/main/java/org/icepear/echarts/render/Engine.java
+++ src/main/java/org/icepear/echarts/render/Engine.java
@@ -5,14 +5,9 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.net.URL;
-import java.net.URLClassLoader;
 
 import com.github.jknack.handlebars.Handlebars;
 import com.github.jknack.handlebars.Template;
-import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
-import com.github.jknack.handlebars.io.FileTemplateLoader;
-import com.github.jknack.handlebars.io.TemplateLoader;
 
 import org.icepear.echarts.component.Option;
 import org.icepear.echarts.serializer.EChartSerializer;
@@ -21,9 +16,7 @@ public class Engine {
     private Handlebars handlebars;
 
     public Engine() throws IOException {
-        TemplateLoader cpLoader = new ClassPathTemplateLoader("/", ".hbs");
-        TemplateLoader fsLoader = new FileTemplateLoader("src/main/java/org/icepear/echarts/render/templates", ".hbs");
-        this.handlebars = new Handlebars().with(cpLoader, fsLoader);
+        this.handlebars = new Handlebars();
     }
 
     public void writeHtml(String html, String path) throws IOException {
