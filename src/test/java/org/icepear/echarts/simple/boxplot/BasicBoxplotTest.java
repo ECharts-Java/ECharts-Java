0e540bc51d419cc0e16c747efd4659dd68b9493a
Zhexin Jin <icepear.jzx@gmail.com>
2022-01-01 17:14:54 -0500

    test simple boxplot

---
+++ src/test/java/org/icepear/echarts/simple/boxplot/BasicBoxplotTest.java
@@ -0,0 +1,38 @@
+package org.icepear.echarts.simple.boxplot;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import org.icepear.echarts.Boxplot;
+import org.icepear.echarts.serializer.EChartsSerializer;
+import org.junit.Test;
+
+public class BasicBoxplotTest {
+    @Test
+    public void testBasicBoxplot() {
+        Boxplot boxplot = new Boxplot()
+                .setTooltip("item")
+                .addXAxis(new String[] { "expr 0", "expr 1", "expr 2", "expr 3", "expr 4" })
+                .addYAxis()
+                .addSeries(new Number[][] {
+                        { 650, 850, 940, 980, 1070 },
+                        { 760, 800, 845, 890, 960 },
+                        { 620, 840, 855, 880, 970 },
+                        { 720, 765, 815, 870, 920 },
+                        { 740, 805, 810, 870, 950 }
+                });
+
+        Reader reader = new InputStreamReader(
+                this.getClass().getResourceAsStream("/simple/boxplot/basic-boxplot.json"));
+        JsonElement expected = JsonParser.parseReader(reader);
+        JsonElement actual = EChartsSerializer.toJsonTree(boxplot.getOption());
+        assertEquals(expected, actual);
+
+        // System.out.println(EChartsSerializer.toJson(boxplot.getOption()));
+    }
+}
---
+++ src/test/java/org/icepear/echarts/simple/boxplot/DataTransformBoxplotTest.java
@@ -0,0 +1,52 @@
+package org.icepear.echarts.simple.boxplot;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import org.icepear.echarts.Boxplot;
+import org.icepear.echarts.charts.boxplot.BoxplotSeries;
+import org.icepear.echarts.charts.boxplot.BoxplotTransform;
+import org.icepear.echarts.charts.boxplot.PrepareBoxplotData;
+import org.icepear.echarts.components.coord.cartesian.CategoryAxis;
+import org.icepear.echarts.components.dataset.Dataset;
+import org.icepear.echarts.serializer.EChartsSerializer;
+import org.junit.Test;
+
+public class DataTransformBoxplotTest {
+    @Test
+    public void testDataTransformBoxplot() {
+        Boxplot boxplot = new Boxplot()
+                .addDataset(new Number[][] {
+                        { 850, 740, 900, 1070, 930, 850, 950, 980, 980, 880, 1000, 980, 930, 650, 760, 810, 1000, 1000,
+                                960, 960 },
+                        { 960, 940, 960, 940, 880, 800, 850, 880, 900, 840, 830, 790, 810, 880, 880, 830, 800, 790, 760,
+                                800 },
+                        { 880, 880, 880, 860, 720, 720, 620, 860, 970, 950, 880, 910, 850, 870, 840, 840, 850, 840, 840,
+                                840 },
+                        { 890, 810, 810, 820, 800, 770, 760, 740, 750, 760, 910, 920, 890, 860, 880, 720, 840, 850, 850,
+                                780 },
+                        { 890, 840, 780, 810, 760, 810, 790, 810, 820, 850, 870, 870, 810, 740, 810, 940, 950, 800, 810,
+                                870 }
+                })
+                .addDataset(new Dataset()
+                        .setTransform(new BoxplotTransform()
+                                .setConfig(new PrepareBoxplotData().setItemNameFormatter("expr {value}"))))
+                .setTooltip("item")
+                .addXAxis(new CategoryAxis())
+                .addYAxis()
+                .addSeries(new BoxplotSeries().setName("boxplot").setDatasetIndex(1));
+
+        Reader reader = new InputStreamReader(
+                this.getClass().getResourceAsStream("/simple/boxplot/data-transform-boxplot.json"));
+        JsonElement expected = JsonParser.parseReader(reader);
+        JsonElement actual = EChartsSerializer.toJsonTree(boxplot.getOption());
+        assertEquals(expected, actual);
+
+        // System.out.println(EChartsSerializer.toJson(boxplot.getOption()));
+    }
+}
