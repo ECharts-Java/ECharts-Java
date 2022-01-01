dc331328242c9f70cf179ada837ac1226ae4412e
Zhexin Jin <icepear.jzx@gmail.com>
2022-01-01 18:26:43 -0500

    test simple gauge

---
+++ src/test/java/org/icepear/echarts/simple/gauge/AnimationGaugeTest.java
@@ -0,0 +1,37 @@
+package org.icepear.echarts.simple.gauge;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import org.icepear.echarts.Gauge;
+import org.icepear.echarts.charts.gauge.GaugeDataItem;
+import org.icepear.echarts.charts.gauge.GaugeDetail;
+import org.icepear.echarts.charts.gauge.GaugeProgress;
+import org.icepear.echarts.charts.gauge.GaugeSeries;
+import org.icepear.echarts.serializer.EChartsSerializer;
+import org.junit.Test;
+
+public class AnimationGaugeTest {
+    @Test
+    public void testAnimationGauge() {
+        Gauge gauge = new Gauge()
+                .setTooltip("item")
+                .addSeries(new GaugeSeries().setName("Pressure")
+                        .setProgress(new GaugeProgress().setShow(true))
+                        .setDetail(new GaugeDetail().setValueAnimation(true))
+                        .setData(new GaugeDataItem[] { new GaugeDataItem().setValue(50).setName("SCORE") }));
+
+        Reader reader = new InputStreamReader(
+                this.getClass().getResourceAsStream("/simple/gauge/animation-gauge.json"));
+        JsonElement expected = JsonParser.parseReader(reader);
+        JsonElement actual = EChartsSerializer.toJsonTree(gauge.getOption());
+        assertEquals(expected, actual);
+
+        // System.out.println(EChartsSerializer.toJson(gauge.getOption()));
+    }
+}
---
+++ src/test/java/org/icepear/echarts/simple/gauge/BasicGaugeTest.java
@@ -0,0 +1,31 @@
+package org.icepear.echarts.simple.gauge;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import org.icepear.echarts.Gauge;
+import org.icepear.echarts.charts.gauge.GaugeDataItem;
+import org.icepear.echarts.serializer.EChartsSerializer;
+import org.junit.Test;
+
+public class BasicGaugeTest {
+    @Test
+    public void testBasicGauge() {
+        Gauge gauge = new Gauge()
+                .setTooltip("item")
+                .addSeries(new GaugeDataItem[] { new GaugeDataItem().setValue(50).setName("SCORE") });
+
+        Reader reader = new InputStreamReader(
+                this.getClass().getResourceAsStream("/simple/gauge/basic-gauge.json"));
+        JsonElement expected = JsonParser.parseReader(reader);
+        JsonElement actual = EChartsSerializer.toJsonTree(gauge.getOption());
+        assertEquals(expected, actual);
+
+        // System.out.println(EChartsSerializer.toJson(gauge.getOption()));
+    }
+}
