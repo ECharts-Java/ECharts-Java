b3f22097955b2a5b6442a71421879f6126738f89
Zhexin Jin <icepear.jzx@gmail.com>
2022-01-01 17:57:41 -0500

    test simple funnel

---
+++ src/test/java/org/icepear/echarts/simple/funnel/BasicFunnelTest.java
@@ -0,0 +1,43 @@
+package org.icepear.echarts.simple.funnel;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import org.icepear.echarts.Funnel;
+import org.icepear.echarts.charts.funnel.FunnelDataItem;
+import org.icepear.echarts.charts.funnel.FunnelLabel;
+import org.icepear.echarts.charts.funnel.FunnelSeries;
+import org.icepear.echarts.serializer.EChartsSerializer;
+import org.junit.Test;
+
+public class BasicFunnelTest {
+    @Test
+    public void testBasicFunnel() {
+        Funnel funnel = new Funnel()
+                .setTitle("Funnel")
+                .setTooltip("item")
+                .setLegend(true)
+                .addSeries(new FunnelSeries()
+                        .setLabel(new FunnelLabel().setShow(true).setPosition("inside"))
+                        .setData(new FunnelDataItem[] {
+                                new FunnelDataItem().setValue(60).setName("Visit"),
+                                new FunnelDataItem().setValue(40).setName("Inquiry"),
+                                new FunnelDataItem().setValue(20).setName("Order"),
+                                new FunnelDataItem().setValue(80).setName("Click"),
+                                new FunnelDataItem().setValue(100).setName("Show")
+                        }));
+
+        Reader reader = new InputStreamReader(
+                this.getClass().getResourceAsStream("/simple/funnel/basic-funnel.json"));
+        JsonElement expected = JsonParser.parseReader(reader);
+        JsonElement actual = EChartsSerializer.toJsonTree(funnel.getOption());
+        assertEquals(expected, actual);
+
+        // System.out.println(EChartsSerializer.toJson(funnel.getOption()));
+    }
+}
