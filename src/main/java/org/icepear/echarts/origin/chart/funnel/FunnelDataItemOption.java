9a4211c99e13d627d408577db61bf1e16c08f0f1
Zhexin Jin <icepear.jzx@gmail.com>
2022-01-01 17:57:41 -0500

    test simple funnel

--- src/main/java/org/icepear/echarts/origin/chart/funnel/FunnelDataItemOption.java
+++ src/main/java/org/icepear/echarts/origin/chart/funnel/FunnelDataItemOption.java
@@ -1,12 +1,12 @@
 package org.icepear.echarts.origin.chart.funnel;
 
-import org.icepear.echarts.origin.util.OptionDataItemObject;
+import org.icepear.echarts.origin.util.NumericOptionDataItemObject;
 import org.icepear.echarts.origin.util.StatesOptionMixin;
 
 /**
  * https://github.com/apache/echarts/blob/566ba8e83dd283ba359fe9aceee0d4a12dbcd6c7/src/chart/funnel/FunnelSeries.ts#L64
  */
-public interface FunnelDataItemOption extends FunnelStateOption, StatesOptionMixin, OptionDataItemObject {
+public interface FunnelDataItemOption extends FunnelStateOption, StatesOptionMixin, NumericOptionDataItemObject {
 
     FunnelDataItemOption setItemStyle(Object itemStyle);
 }
