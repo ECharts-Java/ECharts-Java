6588b8729014f7f84241bd12c1f512d33c52b5f1
Zhexin Jin <icepear.jzx@gmail.com>
2022-01-01 17:14:54 -0500

    test simple boxplot

--- src/main/java/org/icepear/echarts/Boxplot.java
+++ src/main/java/org/icepear/echarts/Boxplot.java
@@ -1,8 +1,6 @@
 package org.icepear.echarts;
 
 import org.icepear.echarts.charts.boxplot.BoxplotSeries;
-import org.icepear.echarts.components.dataset.DataTransform;
-import org.icepear.echarts.components.dataset.Dataset;
 
 public class Boxplot extends CartesianCoordChart<Boxplot, BoxplotSeries> {
     public Boxplot() {
@@ -12,14 +10,5 @@ public class Boxplot extends CartesianCoordChart<Boxplot, BoxplotSeries> {
     @Override
     protected BoxplotSeries createSeries() {
         return new BoxplotSeries().setType("boxplot");
-    }
-
-    @Override
-    protected BoxplotSeries createSeries(Object series) {
-        datasets.add(new Dataset().setSource(series));
-        datasets.add(new Dataset()
-                .setFromDatasetIndex(datasets.size() - 1)
-                .setTransform(new DataTransform().setType("boxplot")));
-        return createSeries().setDatasetIndex(datasets.size() - 1);
     }
 }
