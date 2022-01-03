e46b096ee43673c069d8fed942c31d1d35cf8cc5
Zhexin Jin <icepear.jzx@gmail.com>
2022-01-03 00:57:07 -0500

    format

--- src/main/java/org/icepear/echarts/origin/chart/sunburst/SunburstSeriesOption.java
+++ src/main/java/org/icepear/echarts/origin/chart/sunburst/SunburstSeriesOption.java
@@ -7,7 +7,8 @@ import org.icepear.echarts.origin.util.SunburstColorByMixin;
 /**
  * https://github.com/apache/echarts/blob/566ba8e83dd283ba359fe9aceee0d4a12dbcd6c7/src/chart/sunburst/SunburstSeries.ts#L120
  */
-public interface SunburstSeriesOption extends SeriesOption, SunburstStateOption, SunburstColorByMixin, CircleLayoutOptionMixin {
+public interface SunburstSeriesOption
+        extends SeriesOption, SunburstStateOption, SunburstColorByMixin, CircleLayoutOptionMixin {
 
     SunburstSeriesOption setType(String type);
 
