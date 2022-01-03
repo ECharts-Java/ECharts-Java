9bb9031d4279b1195d4edfabc3f7355e7ac0142d
Zhexin Jin <icepear.jzx@gmail.com>
2022-01-03 03:44:42 -0500

    remove addSeries(PieDataItem[])

--- src/main/java/org/icepear/echarts/Pie.java
+++ src/main/java/org/icepear/echarts/Pie.java
@@ -1,16 +1,10 @@
 package org.icepear.echarts;
 
-import org.icepear.echarts.charts.pie.PieDataItem;
 import org.icepear.echarts.charts.pie.PieSeries;
 
 public class Pie extends Chart<Pie, PieSeries> {
     public Pie() {
         super(Pie.class, PieSeries.class);
-    }
-
-    public Pie addSeries(PieDataItem[] series) {
-        this.series.add(createSeries(series));
-        return this;
     }
 
     @Override
