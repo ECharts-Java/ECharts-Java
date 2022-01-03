48ff4da696d6cf6aeaff1e3f450c011672dc98e0
Zhexin Jin <icepear.jzx@gmail.com>
2022-01-03 00:08:35 -0500

    test simple sankey

---
+++ src/main/java/org/icepear/echarts/origin/chart/sankey/SankeyEmphasisOption.java
@@ -0,0 +1,12 @@
+package org.icepear.echarts.origin.chart.sankey;
+
+import org.icepear.echarts.origin.util.EmphasisOption;
+
+/**
+ * https://github.com/apache/echarts/blob/566ba8e83dd283ba359fe9aceee0d4a12dbcd6c7/src/chart/sankey/SankeySeries.ts#L63
+ * https://github.com/apache/echarts/blob/790687df55a5dbe286e52cf182c0983938efd367/src/util/types.ts#L1541
+ */
+public interface SankeyEmphasisOption extends EmphasisOption {
+
+    SankeyEmphasisOption setFocus(String focus);
+}
