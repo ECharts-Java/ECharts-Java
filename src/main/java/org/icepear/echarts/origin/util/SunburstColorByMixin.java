2096695dbcf6a11de87e99d58f9f429887db0445
Zhexin Jin <icepear.jzx@gmail.com>
2022-01-10 03:11:24 -0500

    fix bug

--- src/main/java/org/icepear/echarts/origin/util/SunburstColorByMixin.java
+++
@@ -1,9 +0,0 @@
-package org.icepear.echarts.origin.util;
-
-/**
- * https://github.com/apache/echarts/blob/566ba8e83dd283ba359fe9aceee0d4a12dbcd6c7/src/util/types.ts#L860
- */
-public interface SunburstColorByMixin {
-
-    SunburstColorByMixin setColorBy(String colorBy);
-}
