8f6a18a5c0be6407462d11352dd64e51638182fd
Xiaochuan Xu <xiaochuanxu@cmu.edu>
2022-08-08 23:58:02 -0700

    formating test files

--- src/test/java/org/icepear/echarts/advanced/treemap/BasicTreemapChartTest.java
+++ src/test/java/org/icepear/echarts/advanced/treemap/BasicTreemapChartTest.java
@@ -17,33 +17,36 @@ import com.google.gson.JsonParser;
 
 public class BasicTreemapChartTest {
 
-    @Test
-    public void testBasicTreemapChart() {
-        TreemapSeriesNodeItem[] nodeAChildren = new TreemapSeriesNodeItem[] {
-                new TreemapSeriesNodeItem().setName("nodeAa").setValue(4),
-                new TreemapSeriesNodeItem().setName("nodeAb").setValue(6)
-        };
-        TreemapSeriesNodeItem[] data = new TreemapSeriesNodeItem[] {
-                new TreemapSeriesNodeItem().setName("nodeA").setValue(10).setChildren(nodeAChildren),
-                new TreemapSeriesNodeItem().setName("nodeB").setValue(20).setChildren(
-                        new TreemapSeriesNodeItem[] {
-                                new TreemapSeriesNodeItem().setName("nodeBa").setValue(20).setChildren(
-                                        new TreemapSeriesNodeItem[] {
-                                                new TreemapSeriesNodeItem().setName("nodeBa1").setValue(20)
-                                        })
-                        })
-        };
+        @Test
+        public void testBasicTreemapChart() {
+                TreemapSeriesNodeItem[] nodeAChildren = new TreemapSeriesNodeItem[] {
+                                new TreemapSeriesNodeItem().setName("nodeAa").setValue(4),
+                                new TreemapSeriesNodeItem().setName("nodeAb").setValue(6)
+                };
+                TreemapSeriesNodeItem[] data = new TreemapSeriesNodeItem[] {
+                                new TreemapSeriesNodeItem().setName("nodeA").setValue(10).setChildren(nodeAChildren),
+                                new TreemapSeriesNodeItem().setName("nodeB").setValue(20).setChildren(
+                                                new TreemapSeriesNodeItem[] {
+                                                                new TreemapSeriesNodeItem().setName("nodeBa")
+                                                                                .setValue(20).setChildren(
+                                                                                                new TreemapSeriesNodeItem[] {
+                                                                                                                new TreemapSeriesNodeItem()
+                                                                                                                                .setName("nodeBa1")
+                                                                                                                                .setValue(20)
+                                                                                                })
+                                                })
+                };
 
-        TreemapSeries series = new TreemapSeries().setType("treemap").setData(data);
-        Option option = new Option().setSeries(new TreemapSeriesOption[] { series });
+                TreemapSeries series = new TreemapSeries().setType("treemap").setData(data);
+                Option option = new Option().setSeries(new TreemapSeriesOption[] { series });
 
-        Reader reader = new InputStreamReader(
-                this.getClass().getResourceAsStream("/advanced/treemap/basic-treemap-chart.json"));
-        JsonElement expected = JsonParser.parseReader(reader);
-        JsonElement actual = new EChartsSerializer().toJsonTree(option);
-        assertEquals(expected, actual);
+                Reader reader = new InputStreamReader(
+                                this.getClass().getResourceAsStream("/advanced/treemap/basic-treemap-chart.json"));
+                JsonElement expected = JsonParser.parseReader(reader);
+                JsonElement actual = new EChartsSerializer().toJsonTree(option);
+                assertEquals(expected, actual);
 
-        // System.out.println(new EChartsSerializer().toJson(option));
-    }
+                // System.out.println(new EChartsSerializer().toJson(option));
+        }
 
 }
