c1a10afa834f6f3f0fca066428ff4f70a94773ab
Markus Gettrup <markus.gettrup@guidecom.de>
2025-04-12 23:55:16 +0200

    Add @Serial annotation

--- src/main/java/org/icepear/echarts/Bar.java
+++ src/main/java/org/icepear/echarts/Bar.java
@@ -2,10 +2,12 @@ package org.icepear.echarts;
 
 import org.icepear.echarts.charts.bar.BarSeries;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 public class Bar extends CartesianCoordChart<Bar, BarSeries> implements Serializable {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
     public Bar() {
