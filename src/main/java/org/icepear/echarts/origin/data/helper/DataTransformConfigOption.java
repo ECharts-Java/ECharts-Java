a7c56ee8a14349ddb07f81523c641f2c9af50661
Xiaochuan Xu <35758677+incandescentxxc@users.noreply.github.com>
2025-05-04 20:16:00 -0400

    Update config and correspondin class (#96)
    * update config and corresponding source class
    
    * add class
    
    * add class

--- src/main/java/org/icepear/echarts/origin/data/helper/DataTransformConfigOption.java
+++ src/main/java/org/icepear/echarts/origin/data/helper/DataTransformConfigOption.java
@@ -2,16 +2,17 @@ package org.icepear.echarts.origin.data.helper;
 
 /**
  * https://echarts.apache.org/examples/en/editor.html?c=data-transform-multiple-pie
+ * https://echarts.apache.org/examples/en/editor.html?c=data-transform-sort-bar
  */
 public interface DataTransformConfigOption {
 
     DataTransformConfigOption setDimension(String dimension);
+
+    DataTransformConfigOption setOrder(String order);
 
     DataTransformConfigOption setValue(Number value);
 
     DataTransformConfigOption setValue(Object value);
 
     DataTransformConfigOption setValue(String value);
-
-    DataTransformConfigOption setOrder(String order);
 }
